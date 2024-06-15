13a055e6d495289be2c9168d9555f3a98e73c53a
Alek4oo <aalekov121@gmail.com>
2024-06-15 18:21:55 +0300

    Adding database

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///fitness.db'
+db = SQLAlchemy(app)
+
+class Workout(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, nullable=False)
+    type = db.Column(db.String(50), nullable=False)
+    duration = db.Column(db.Integer, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
 
 @app.route('/')
 def index():
